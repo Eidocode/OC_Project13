b83afc1d3c7e4b4ceb8fb08c03be70ef69501d05
David Bouzerar <eidocode@orange.fr>
2023-01-02 22:13:27 +0100

    - forgotten to commit and push product_user/views.py in the latest version

---
+++ product_user/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from product.models import DeviceUser
+
+
+def show_product_user(request):
+    """
+    Used for product_user page
+    """
+    users = DeviceUser.objects.filter().order_by('-id')[:10]
+
+    context = {
+        'device_users': users
+    }
+    return render(request, 'product_user.html', context)
