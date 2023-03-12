573568a3465aa69764e51f52af9a4f5307d167af
David Bouzerar <eidocode@orange.fr>
2023-03-12 20:55:49 +0100

    - Added test device_list html template for jquery dashboard testing - Added Jquery to the device-select form. This allows for updating the data based on the selected category - Modified the "dashboard views.py" to send JSON data for use with Jquery

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^', views.dashboard, name='dashboard'),
+    url(r'^(?P<device_type>[\w ]+)/$', views.dashboard, name='dashboard_filter'),
+    url(r'^$', views.dashboard, name='dashboard'),
+
+    # url(r'^devices/(?P<device_type>\w+)/$', views.device_list, name='device_list'),
+    # url(r'^devices/$', views.device_list, name='device_list_all'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,13 +1,47 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
 
-from product.models import Device
+from product.models import Device, Category
 
 
-def dashboard(request):
-    devices_qs = Device.objects.filter()
+def dashboard(request, device_type=None):
+    devices_qs = Device.objects.all()
+    categories_qs = Category.objects.all()
+
+    if device_type == 'All devices':
+        data = [{'name': device.inventory.hostname} for device in devices_qs]
+        return JsonResponse(data, safe=False)
+
+    if device_type:
+        category = get_object_or_404(Category, name=device_type)
+        devices = Device.objects.filter(product__category=category)
+        data = [{'name': device.inventory.hostname} for device in devices]
+        return JsonResponse(data, safe=False)
 
     context = {
-        'devices_qs': devices_qs,
+        'devices': devices_qs,
+        'categories': categories_qs,
     }
 
+    if request.is_ajax():
+        return JsonResponse(context)
+
     return render(request, 'dashboard/dashboard.html', context)
+
+
+# def device_list(request, device_type=None):
+#     if device_type:
+#         category = get_object_or_404(Category, name=device_type)
+#         devices = Device.objects.filter(product__category=category)
+#         data = [{'name': device.inventory.hostname} for device in devices]
+#         return JsonResponse(data, safe=False)
+#
+#     devices_qs = Device.objects.all()
+#     categories_qs = Category.objects.all()
+#
+#     context = {
+#         'devices': devices_qs,
+#         'categories': categories_qs,
+#     }
+#
+#     return render(request, 'dashboard/device_list.html', context)
